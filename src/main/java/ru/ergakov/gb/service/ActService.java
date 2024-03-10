465bde0ec44b446dd8fddaee3e6e01cf5e3705f0
Ierga <i.ergakoff@gmail.com>
2024-03-10 19:41:04 +0300

    GB-Spring. Homework 12. Start

---
+++ src/main/java/ru/ergakov/gb/service/ActService.java
@@ -0,0 +1,16 @@
+package ru.ergakov.gb.service;
+
+
+import ru.ergakov.gb.model.Act;
+
+import java.util.List;
+
+
+public interface ActService {
+    List<Act> getAllActs();
+    Act createAct(Act note);
+    Act getActById(Long id);
+    Act updateAct(Act act);
+    void deleteAct(Long id);
+    List<Act> findActByReportingPeriod(String reportingPeriod);
+}
